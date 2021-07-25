33bfa718081b7ebaf2fcefd9e1d22245247b3316
Bryan Casanelli <bcasanelli@dparadig.com / bryancasanelli@gmail.com>
2021-07-24 22:30:30 -0400

    Project name changed.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PENELOPE-GUI
+# PENELOPE-Extended
 Graphic User Interface for the Monte Carlo code PENELOPE.
 
 This project is not usable yet.
---
+++ app/src/main/java/penelope/extended/App.java
@@ -0,0 +1,41 @@
+package PENELOPE.GUI;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+/**
+ * Main class.
+ */
+public class App extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /**
+     * Start method.
+     */
+    @Override
+    public void start(Stage primaryStage) {
+        // Window title
+        primaryStage.setTitle("PENELOPE GUI");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+}
