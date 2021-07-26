4deccf80d29bcf807051e99a2b5c4241e70e6b24
Bryan Casanelli <bryancasanelli@gmail.com>
2021-07-25 22:00:51 -0400

    Added functionality to the language menu.

--- app/src/main/java/penelope/extended/TopPanel.java
+++ app/src/main/java/penelope/extended/TopPanel.java
@@ -1,5 +1,7 @@
 package penelope.extended;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
@@ -14,11 +16,41 @@ public class TopPanel {
      */
     public MenuBar generate(){
         // Language
-        Menu menuLanguage = new Menu(Language.get(0));
+        Menu menuLanguage = new Menu(Language.get(1));
         for (String key: Language.available.keySet()){
             MenuItem menuItem = new MenuItem(Language.available.get(key));
+            menuItem.setOnAction(new LanguageSelected(key));
             menuLanguage.getItems().add(menuItem);
         }
         return new MenuBar(menuLanguage);
+    }
+
+    /**
+     * Language actions.
+     */
+    class LanguageSelected implements EventHandler<ActionEvent>{
+
+        // The new language to be setted.
+        private String lang;
+
+        /**
+         * Initializer.
+         * @param key (String) Lenguage to be setted.
+         */
+        public LanguageSelected(String key){
+            lang = key;
+        }
+
+        /**
+         * Changes the language.
+         */
+        @Override
+        public void handle(ActionEvent event) {
+            Language.changeLanguage(lang);
+            if (!lang.equals(Language.lang)){
+                MessageDialog dialog = new MessageDialog(Language.get(2));
+                dialog.showAndWait();
+            }
+        }
     }
 }
